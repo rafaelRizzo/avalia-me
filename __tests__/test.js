04a641137cefd3c1695516e039aed2bb4c0b7c99
Rafael Rizzo <rafaelbreschi4@gmail.com>
2024-12-12 14:15:23 -0300

    feat: ajustes nos testes e remocao de pacotes nao usados

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,41 +1,36 @@
-import fetch from 'node-fetch'; // Usando node-fetch para fazer requisições HTTP
+import axios from 'axios'; // Usando axios para fazer requisições HTTP
 import { expect } from 'chai'; // Chai para asserções
-import { createServer } from 'http'; // Para iniciar um servidor HTTP
-import AvaliacaoController from '../src/controllers/AvaliacaoController.js'; // Ajuste conforme necessário
 import { logger } from './../src/logger/index.js';
 
-describe('Testando a rota CREATE para avaliação', () => {
-    let testServer;
+const API_URL = 'http://localhost:3000'; // Porta padrão do servidor
 
-    before(() => {
-        // Inicia o servidor antes de rodar os testes
-        testServer = createServer(AvaliacaoController);
-        testServer.listen(3001);
-    });
-
-    after(() => {
-        // Fecha o servidor após os testes
-        testServer.close();
-    });
+describe('Testando a rota CREATE para avaliação', function () {
+    // Aumenta o tempo limite do teste para evitar falhas por timeout
+    this.timeout(5000);
 
     it('Deve gerar uma avaliação e retornar status 201', async () => {
         const body = {
             nome_atendente: 'João Silva',
             nome_empresa: 'Empresa XYZ',
-            // ip_generated: '192.168.0.1',
-            protocolo_atendimento: '123456789'
+            ip_generated: '1.1.1.1',
+            protocolo_atendimento: '123456789',
         };
 
-        const response = await fetch('http://localhost:3000/generate', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(body)
-        });
+        try {
+            const response = await axios.post(`${API_URL}/generate`, body, {
+                headers: { 'Content-Type': 'application/json' },
+            });
 
-        const data = await response.json();
-        logger.info("data", data);
-        expect(response.status).to.equal(201);
-        expect(data.message).to.equal('Avaliação criada com sucesso');
-        expect(data).to.have.property('uuid'); // Verifica se o UUID foi gerado
+            const data = response.data;
+            logger.info('Resposta da API:', data);
+
+            // Validações
+            expect(response.status).to.equal(201);
+            expect(data.message).to.equal('Avaliação criada com sucesso');
+            expect(data).to.have.property('uuid'); // Verifica se o UUID foi gerado
+        } catch (error) {
+            logger.error('Erro na requisição:', error.response?.data || error.message);
+            throw error;
+        }
     });
 });
