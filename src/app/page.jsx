141ce4665304d93e9d55e9793414636b9e50913b
Rafael Rizzo <rafaelbreschi4@gmail.com>
2024-12-16 15:46:55 -0300

    fix: ajuste no tamanho da fonte

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
         <div className="flex flex-col min-h-screen items-center justify-center text-center p-5">
-            <h1>Ops! algo errado aconteceu</h1>
+            <h1 className="font-bold text-3xl">Ops! algo errado aconteceu</h1>
             <i className="text-muted-foreground">Não era para você estar aqui, verifique o id da sua avaliação e tente novamente</i>
         </div>
     )
